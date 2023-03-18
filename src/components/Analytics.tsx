b1abb6be89adc7e1d5cfd1bf8baadd6cd6225929
Cullen Hutchison <cullen@Cullens-Air.attlocal.net>
2023-03-17 20:07:53 -0500

    Add analytics tsx

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { DiscordDataPackage } from '../types'
+
+type Props = {
+  dataPackage: DiscordDataPackage
+}
+
+export const Analytics: React.FunctionComponent<Props> = ({
+  dataPackage,
+}: Props) => {
+  return (
+    <div>
+      <h1>Analytics</h1>
+    </div>
+  )
+}
